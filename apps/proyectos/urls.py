f0942207d418fd851c9f527cf5dc84742d4da099
Tatiana <kren_tatiana@hotmail.com>
2025-08-27 13:45:11 -0500

    FEAT: Crear GET de proyecto

--- apps/proyectos/urls.py
+++ apps/proyectos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from apps.proyectos.api.proyecto import ProyectoAPIView
 
 urlpatterns = [
+    path("proyectos/", ProyectoAPIView.as_view(), name="proyectos-list"),
     path('proyectos/crear/', ProyectoAPIView.as_view(), name='proyecto-crear'),
     path('proyectos/<uuid:id>/', ProyectoAPIView.as_view(), name='proyecto-editar'),
 ]
