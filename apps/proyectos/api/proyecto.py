7e7110a67683b8966e627644d7f72f1ef2b75ea3
Tatiana <kren_tatiana@hotmail.com>
2025-09-11 10:45:57 -0500

    FEAT: Agregar configuracion para consulta de proyecto desde ETL

--- apps/proyectos/api/proyecto.py
+++ apps/proyectos/api/proyecto.py
@@ -5,7 +5,10 @@ from rest_framework.response import Response
 from rest_framework import generics, status
 from apps.base.api.filtros import PaginacionEstandar
 from apps.proyectos.api.filtros import ProyectoFilter
+import os
 
+
+API_TOKEN = os.getenv("TOKEN_PROYECTO")
 
 class ProyectoAPIView(generics.GenericAPIView):
     queryset = Proyecto.objects.all()
@@ -13,6 +16,19 @@ class ProyectoAPIView(generics.GenericAPIView):
     lookup_field = 'id'
     pagination_class = PaginacionEstandar
     filterset_class = ProyectoFilter
+
+    def _validar_token(self, request):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return False, "Token no enviado o inválido"
+
+        token = auth_header.split(" ")[1]
+
+        if token != settings.API_TOKEN:
+            return False, "Token no autorizado"
+
+        return True, "Token válido"
 
     def get(self, request, *args, **kwargs):
         proyectos = self.filter_queryset(self.get_queryset())
