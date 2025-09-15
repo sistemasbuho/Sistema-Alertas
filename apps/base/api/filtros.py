48927004d586930b992693cd5945eef8f73e9bef
Tatiana <kren_tatiana@hotmail.com>
2025-09-15 07:42:30 -0500

    FIX: Cambiar tipo de busqueda en listados

--- apps/base/api/filtros.py
+++ apps/base/api/filtros.py
@@ -7,24 +7,27 @@ from rest_framework.pagination import PageNumberPagination
 class RedesFilter(django_filters.FilterSet):
     fecha_inicio = django_filters.DateFilter(field_name="fecha", lookup_expr="gte")
     fecha_fin = django_filters.DateFilter(field_name="fecha", lookup_expr="lte")
-    autor = django_filters.CharFilter(field_name="autor", lookup_expr="icontains")
-    url = django_filters.CharFilter(field_name="url", lookup_expr="icontains")
-    proyecto = django_filters.CharFilter(field_name="proyecto__nombre", lookup_expr="icontains")
+    autor = django_filters.CharFilter(field_name="autor", lookup_expr="istartswith")
+    url = django_filters.CharFilter(field_name="url", lookup_expr="istartswith")
+    proyecto = django_filters.CharFilter(field_name="proyecto__nombre", lookup_expr="istartswith")
+    estado_enviado = django_filters.BooleanFilter(field_name="detalles_envio__estado_enviado")
 
     class Meta:
         model = Redes
-        fields = ["autor", "url", "proyecto", "fecha_inicio", "fecha_fin"]
+        fields = ["autor", "url", "proyecto", "fecha_inicio", "fecha_fin", "estado_enviado"]
 
 class MediosFilter(django_filters.FilterSet):
     fecha_inicio = django_filters.DateFilter(field_name="fecha", lookup_expr="gte")
     fecha_fin = django_filters.DateFilter(field_name="fecha", lookup_expr="lte")
-    medio = django_filters.CharFilter(field_name="medio", lookup_expr="icontains")
-    ciudad = django_filters.CharFilter(field_name="ciudad", lookup_expr="icontains")
-    proyecto = django_filters.CharFilter(field_name="proyecto__nombre", lookup_expr="icontains")
+    medio = django_filters.CharFilter(field_name="medio", lookup_expr="istartswith")
+    ciudad = django_filters.CharFilter(field_name="ciudad", lookup_expr="istartswith")
+    proyecto = django_filters.CharFilter(field_name="proyecto__nombre", lookup_expr="istartswith")
+    estado_enviado = django_filters.BooleanFilter(field_name="detalles_envio__estado_enviado")
+
 
     class Meta:
         model = Articulo
-        fields = ["medio", "ciudad", "proyecto", "fecha_inicio", "fecha_fin"]
+        fields = ["medio", "ciudad", "proyecto", "fecha_inicio", "fecha_fin","estado_enviado"]
 
 
 class DetalleEnvioFilter(django_filters.FilterSet):
