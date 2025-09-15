dc0f921a5b3525e1c649739948a7a5d6647ee3e1
Tatiana <kren_tatiana@hotmail.com>
2025-09-15 08:00:57 -0500

    FIX: Cambiar tipo de busqueda en listados - Redes

--- apps/base/api/filtros.py
+++ apps/base/api/filtros.py
@@ -20,6 +20,9 @@ class MediosFilter(django_filters.FilterSet):
     fecha_inicio = django_filters.DateFilter(field_name="fecha", lookup_expr="gte")
     fecha_fin = django_filters.DateFilter(field_name="fecha", lookup_expr="lte")
     medio = django_filters.CharFilter(field_name="medio", lookup_expr="istartswith")
+    url = django_filters.CharFilter(field_name="url", lookup_expr="istartswith")
+    autor = django_filters.CharFilter(field_name="autor", lookup_expr="istartswith")
+
     ciudad = django_filters.CharFilter(field_name="ciudad", lookup_expr="istartswith")
     proyecto = django_filters.CharFilter(field_name="proyecto__nombre", lookup_expr="istartswith")
     estado_enviado = django_filters.BooleanFilter(field_name="detalles_envio__estado_enviado")
@@ -27,7 +30,7 @@ class MediosFilter(django_filters.FilterSet):
 
     class Meta:
         model = Articulo
-        fields = ["medio", "ciudad", "proyecto", "fecha_inicio", "fecha_fin","estado_enviado"]
+        fields = ["medio", "url","ciudad", "proyecto", "fecha_inicio", "fecha_fin","estado_enviado","autor"]
 
 
 class DetalleEnvioFilter(django_filters.FilterSet):
